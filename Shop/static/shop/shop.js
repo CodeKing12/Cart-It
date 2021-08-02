1a9dd17797537dc5b59bf143956fc80544e527b4
Kingsley <eyetukingsley330@gmail.com>
2021-08-02 13:03:50 +0100

    Create cards for each product on the site

--- Shop/static/shop/shop.js
+++ Shop/static/shop/shop.js
@@ -1,15 +1,21 @@
+// When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("sticknav");
+// Get the navbar
+var navbar = document.getElementById("mobilestick");
+
+// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
-}
+} 
+
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "150px";
@@ -21,25 +27,13 @@ function closeNav() {
 //  document.getElementById("main").style.marginLeft= "0";
 }
 
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementByClass("sticknav");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 /* Open when someone clicks on the span element */
-function openNav() {
+function openMobileNav() {
   document.getElementById("myNav").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
+function closeMobileNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+ 
