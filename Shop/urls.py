d69f68cc8b79c1238dd8c81792e54bca595cba24
Kingsley <eyetukingsley330@gmail.com>
2021-08-04 20:38:03 +0100

    Shop page is done. About to delete all products to fix a url problem

--- Shop/urls.py
+++ Shop/urls.py
@@ -4,7 +4,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.shophome, name="shop_home"),
-    path("<categories>", views.category_items, name="category_items")
+    path("product_info/<product_id>", views.product_details, name="product_details"),
+    path("<categories>", views.category_items, name="category_items"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- Shop/views.py
+++ Shop/views.py
@@ -6,10 +6,15 @@ from django.db.models import Q
 def shophome(request):
     all_categories = Categories.objects.all()
     all_products = Product.objects.all()
-    return render(request, 'shop/home.html', {'categories': all_categories, 'all_products': all_products})
+    context = {'categories': all_categories, 'all_products': all_products}
+    return render(request, 'shop/home.html', context=context)
 
 def category_items(request, categories):
     cat_id = Categories.objects.get(categories=categories)
     cid = cat_id.id
     category_list = Product.objects.filter(category=cid)
     return render(request, "shop/category_products.html", {"category_list": category_list})
+
+def product_details(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return render(request, "shop/product_info.html", {"product": product})
