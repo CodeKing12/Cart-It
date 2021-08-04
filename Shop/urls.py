8f63dbdbbcf5a29156af8bfff952d90fc40cc77b
Kingsley <eyetukingsley330@gmail.com>
2021-08-04 20:49:39 +0100

    I'm done with the shop home page. Moving on to the purchase page now

--- Shop/urls.py
+++ Shop/urls.py
@@ -4,7 +4,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.shophome, name="shop_home"),
-    path("product_info/<product_id>", views.product_details, name="product_details"),
     path("<categories>", views.category_items, name="category_items"),
 ]
 
