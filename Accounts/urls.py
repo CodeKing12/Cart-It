cc0c64db698b875d9c0447180e5f32ca1fb72fda
Kingsley <eyetukingsley330@gmail.com>
2021-08-18 03:40:34 +0100

    The signup page is done and looking very beautiful. Next thing to do is to style the button once more and create a login page.

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -4,7 +4,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("create_account", views.account_creation_view, name="create_an_account"),
-    path("confirm_email", views.email_confirm, name="confirm_email")
+    path("confirm_email", views.email_confirm, name="confirm_email"),
+    path("login", views.login_view, name="login_account")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
